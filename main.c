1675d9e42ad87381d80fa7be24769769eac78b43
Nicholas Copic <Nicholas Copic>
2016-03-23 22:31:06 -0700

    Most changes were made in SET_DIRECTION, SET_PWM and I added an ISR.

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 //  This comment is a test for the github repository. test again.
 // File:         lab2p1.c
 // Date:         
-// Authors:      
+// Authors:     #Nick is attempting to make a contribution to this group. Woo! 
+//              All comments by me will have a '#' sign at the beginning 
 //
 // Description: Part 1 for lab 1
 // ******************************************************************************************* //
@@ -23,10 +24,13 @@
 
 
 typedef enum state_enum {
-    INIT, READ_ADC, SET_PWM, SET_DIRECTION, PRINT_LCD, FORWARD, REVERSE
+    //defines the possible states for the FSM
+    INIT, READ_ADC, SET_PWM, SET_DIRECTION, PRINT_LCD, FORWARD, REVERSE, IDLE 
 } state_t;
 
 
+volatile state_t direction = FORWARD;   //#Variable to tell us what direction the robot needs to go (used in ISR)
+volatile int dir_count = 0;             //#Variable used to tell us where to go from the IDLE state (used in ISR)
 volatile state_t myState;
 volatile int read;
 volatile int read_reset;    /*integer to store value of the reset button*/
@@ -36,7 +40,7 @@ volatile char timer_flag;   /*The timer flag increments every 10ms*/
  */
 int main(void)
 {
-    state_t direction = FORWARD;
+    //state_t direction = FORWARD;                  //#Nick commented this out 
     int voltageADC = 0;
     int lastVoltage = 0;
     char charToWrite = 0;
@@ -58,40 +62,80 @@ int main(void)
                 myState = READ_ADC;
                 break;
             case READ_ADC:
-                voltageADC = ADC1BUF0;
+                voltageADC = ADC1BUF0;  //0-1023
                 delayUs(1000);
                 myState = SET_DIRECTION;
                 break;
             case SET_DIRECTION:
-                if(voltageADC >= 512) {
-                    direction = FORWARD;
-                    LATGbits.LATG13 = ENABLED;
-                    LATDbits.LATD5 = DISABLED;
-                    LATGbits.LATG15 = ENABLED;
-                    LATDbits.LATD11 = DISABLED;
-                }
-                else {
-                    direction = REVERSE;
+                if(direction == FORWARD) {
+                    //direction = FORWARD;                      //#No longer need this line 
+                    LATDbits.LATD5 = DISABLED;                  //#Assuming that D5 moves it backward when set high 
+                    LATDbits.LATD11 = DISABLED;                 //#Assuming that D11 moves it backward when set high 
+                    LATGbits.LATG13 = ENABLED;                  //#Assuming that G13 moves it forward when set high 
+                    LATGbits.LATG15 = ENABLED;                  //#Assuming that G15 moves it forward when set high 
+                                                                //#Assuming(D5 and G13)are a pair and(D11 and G15)are a pair 
+                    }
+                else if (direction == REVERSE){
+                    //direction = REVERSE;                      //#No longer need this line 
+                    LATDbits.LATD5 = ENABLED;
                     LATDbits.LATD11 = ENABLED;
                     LATGbits.LATG13 = DISABLED;
-                    LATDbits.LATD5 = ENABLED;
                     LATGbits.LATG15 = DISABLED;
                     
                 }
-                
+                else {
+                    //#if not FORWARD or REVERSE we must be in IDLE.
+                    LATDbits.LATD5 = DISABLED;
+                    LATDbits.LATD11 = DISABLED;
+                    LATGbits.LATG13 = DISABLED;
+                    LATGbits.LATG15 = DISABLED;
+
+                }
                 myState = SET_PWM;
                 break;
             case SET_PWM:
                 
                 if(direction == FORWARD) {
-                    //We know the ADC is greater than or equal to 512
                     OC3RS = voltageADC;
                     OC4RS = voltageADC;
+
+                    //#let OC3RS be X
+                    //#let OC4RS be 1-X equivalent on the pot
+                    
+                    if (voltageADC == 511){
+                        //#both motors are operate at full speed ahead 
+                    }
+                    else if(voltageADC > 511){
+                        //#OC3RS = full speed ahead
+                        //#OC4RS = fractional speed ahead
+                    }
+                    else {
+                        //#voltageADC must be < 511
+                        //OC3RS = fractional speed ahead
+                        //OC4RS = full speed ahead
+                    }
+
                 }
                 if(direction == REVERSE) {
-                    //We know the ADC is less than 512
+                    
                     OC3RS = 1023 - voltageADC;
                     OC4RS = 1023 - voltageADC;
+                    
+                    //#if the robot turn the wrong way when in reverse 
+                    //when we first test it, just flip 
+                    //what gets assigned to the OC3RS and OC4RS
+                    if (voltageADC == 511){
+                        //#both motors are operate at full speed backwards 
+                    }
+                    else if(voltageADC > 511){
+                        //#OC3RS = full speed backwards
+                        //#OC4RS = fractional speed backwards
+                    }
+                    else {
+                        //#voltageADC must be < 511
+                        //#OC3RS = fractional speed backwards
+                        //#OC4RS = full speed backwards
+                    }
                 }
                 
                 if(voltageADC != lastVoltage) {
@@ -153,3 +197,31 @@ void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt(void){
         myState = myState;
     }
 }
+
+//# Totes need to create an ISR for when the button gets pressed. 
+//  IDK how to do that tho... this is my best guess
+//
+// void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt(void){
+//     //puts flag down
+//     switch(direction) {
+//          case(FORWARD):
+//              direction = IDLE;
+//              dir_count = 1;
+//              break;
+//          case(BACKWARD):
+//              direction = IDLE;
+//              dir_count = 0;
+//              break;
+//          case(IDLE):
+//              if(count == 0){
+//                  direction = FORWARD;
+//              }
+//              else {
+//                  //count must be 1.
+//                  direction = BACKWARD;
+//              }
+//              break;
+//          }
+// }
+//
+//
