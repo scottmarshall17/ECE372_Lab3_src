1fb032feb9e551a98dbaa06634cc996ccdc59285
Scott Marshall <scottjmar2@gmail.com>
2016-03-25 11:35:02 -0700

    Working build part 2

--- button.c
+++ button.c
@@ -11,10 +11,11 @@
 
 void initButton(void) {
     //asm volatile("di");h
-    TRISDbits.TRISD6 = INPUT;   //enable the button pin as an input
+    //Used to be D6, now D2
+    TRISDbits.TRISD7 = INPUT;   //enable the button pin as an input
     CNCONDbits.ON = 1;
-    CNENDbits.CNIED6 = 1;       // Enable interrupt for button
-    CNPUDbits.CNPUD6 = 1;        // Enable Internal Pull-up resistor
+    CNENDbits.CNIED7 = 1;       // Enable interrupt for button
+    CNPUDbits.CNPUD7 = 1;        // Enable Internal Pull-up resistor
     IPC8bits.CNIP = 7; //CHANGED THIS!!!!
     IFS1bits.CNDIF = 0;         // Put Interrupt flag down
     IEC1bits.CNDIE = 1;         // Enable overall CN Interrupt
--- main.c
+++ main.c
@@ -25,22 +25,23 @@
 
 typedef enum state_enum {
     //defines the possible states for the FSM
-    INIT, READ_ADC, SET_PWM, SET_DIRECTION, PRINT_LCD, FORWARD, REVERSE, IDLE 
+    INIT, READ_ADC, SET_PWM, SET_DIRECTION, CHANGE_DIRECTION, PRINT_LCD, FORWARD, REVERSE, IDLE 
 } state_t;
 
 
-volatile state_t direction = FORWARD;   //#Variable to tell us what direction the robot needs to go (used in ISR)
-volatile int dir_count = 0;             //#Variable used to tell us where to go from the IDLE state (used in ISR)
+//volatile state_t direction = FORWARD;   //#Variable to tell us what direction the robot needs to go (used in ISR)
+//volatile int dir_count = 0;             //#Variable used to tell us where to go from the IDLE state (used in ISR)
 volatile state_t myState;
 volatile int read;
 volatile int read_reset;    /*integer to store value of the reset button*/
-volatile char timer_flag;   /*The timer flag increments every 10ms*/
+volatile char button_flag;   /*The timer flag increments every 10ms*/
 /* Please note that the configuration file has changed from lab 0.
  * the oscillator is now of a different frequency.
  */
 int main(void)
 {
-    //state_t direction = FORWARD;                  //#Nick commented this out 
+    state_t direction = FORWARD;                  //#Nick commented this out 
+    state_t nextDirection = REVERSE;
     int voltageADC = 0;
     int lastVoltage = 0;
     char charToWrite = 0;
@@ -48,6 +49,7 @@ int main(void)
     SYSTEMConfigPerformance(10000000);
     enableInterrupts();
     myState = INIT;
+    initButton();
     initLEDs();
     initTimers();
     initLCD();
@@ -64,7 +66,14 @@ int main(void)
             case READ_ADC:
                 voltageADC = ADC1BUF0;  //0-1023
                 delayUs(1000);
+                
+                if(myState == CHANGE_DIRECTION) {
+                    myState = CHANGE_DIRECTION;
+                    break;
+                }
+                
                 myState = SET_DIRECTION;
+                
                 break;
             case SET_DIRECTION:
                 if(direction == FORWARD) {
@@ -91,53 +100,32 @@ int main(void)
                     LATGbits.LATG15 = DISABLED;
 
                 }
+                if(myState == CHANGE_DIRECTION) {
+                    myState = CHANGE_DIRECTION;
+                    break;
+                }
                 myState = SET_PWM;
                 break;
             case SET_PWM:
                 
-                if(direction == FORWARD) {
-                    OC3RS = voltageADC;
-                    OC4RS = voltageADC;
+                //OC3RS = voltageADC;
+                //OC4RS = voltageADC;
 
-                    //#let OC3RS be X
-                    //#let OC4RS be 1-X equivalent on the pot
-                    
-                    if (voltageADC == 511){
-                        //#both motors are operate at full speed ahead 
-                    }
-                    else if(voltageADC > 511){
-                        //#OC3RS = full speed ahead
-                        //#OC4RS = fractional speed ahead
-                    }
-                    else {
-                        //#voltageADC must be < 511
-                        //OC3RS = fractional speed ahead
-                        //OC4RS = full speed ahead
-                    }
+                //#let OC3RS be X
+                //#let OC4RS be 1-X equivalent on the pot
 
+                if(voltageADC >= 511){
+                    OC3RS = 1023;//#OC3RS = full speed ahead
+                    OC4RS = 1023 - (voltageADC - 511);//#OC4RS = fractional speed ahead
                 }
-                if(direction == REVERSE) {
-                    
-                    OC3RS = 1023 - voltageADC;
-                    OC4RS = 1023 - voltageADC;
-                    
-                    //#if the robot turn the wrong way when in reverse 
-                    //when we first test it, just flip 
-                    //what gets assigned to the OC3RS and OC4RS
-                    if (voltageADC == 511){
-                        //#both motors are operate at full speed backwards 
-                    }
-                    else if(voltageADC > 511){
-                        //#OC3RS = full speed backwards
-                        //#OC4RS = fractional speed backwards
-                    }
-                    else {
-                        //#voltageADC must be < 511
-                        //#OC3RS = fractional speed backwards
-                        //#OC4RS = full speed backwards
-                    }
+                else {
+                    //#voltageADC must be < 511
+                    OC3RS = 512 + voltageADC;//OC3RS = fractional speed ahead
+                    OC4RS = 1023;//OC4RS = full speed ahead
                 }
-                
+                if(myState == CHANGE_DIRECTION) {
+                    myState = CHANGE_DIRECTION;
+                }
                 if(voltageADC != lastVoltage) {
                     lastVoltage = voltageADC;
                     myState = PRINT_LCD;
@@ -155,6 +143,28 @@ int main(void)
                 delayUs(30000);
                  */
                 printVoltage(voltageADC);
+                if(myState == CHANGE_DIRECTION) {
+                    myState = CHANGE_DIRECTION;
+                    break;
+                }
+                myState = READ_ADC;
+                break;
+            case CHANGE_DIRECTION:
+                if(direction == FORWARD) {
+                    direction = IDLE;
+                }
+                else if(direction == REVERSE) {
+                    direction = IDLE;
+                }
+                else if(direction == IDLE) {
+                    direction = nextDirection;
+                    if(nextDirection == FORWARD) {
+                        nextDirection = REVERSE;
+                    }
+                    else {
+                        nextDirection = FORWARD;
+                    }
+                }
                 myState = READ_ADC;
                 break;
         }
@@ -177,20 +187,21 @@ void __ISR(_TIMER_1_VECTOR, IPL7SRS) timer1Handler(void){
 
 void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt(void){
     //TODO: Implement the interrupt to capture the press of the button
-    PORTB;
-    IFS1bits.CNBIF = FLAG_DOWN;
+    PORTD;
+    IFS1bits.CNDIF = FLAG_DOWN;
     IFS0bits.T4IF = 0;      //lower timer 4 flag for delay
     TMR4 = 0;  
-    PR4 = 10000;//manually clear timer 4 register
+    PR4 = 15000;//manually clear timer 4 register
     T4CONbits.ON = 1;       //turn timer 4 on.
     while(IFS0bits.T4IF != 1){};    //5ms delay
     T4CONbits.ON = 0;       //turn off timer 4
     IFS0bits.T4IF = 0;
-    read = PORTBbits.RB12;
-    if(1){
+    read = PORTD;
+    read = PORTDbits.RD7;
+    if(read == 0){
         switch(myState) {
             default:
-                myState = myState;
+                myState = CHANGE_DIRECTION;
                 break;
         }
     } else {
